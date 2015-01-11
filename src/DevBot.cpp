0134a0570ead669ba7c31c5f81195b1b5cd132ec
Liam <liam.tab@gmail.com>
2015-01-10 16:34:17 -0800

    Push

--- src/DevBot.cpp
+++ src/DevBot.cpp
@@ -2,22 +2,27 @@
 
 DevBot :: DevBot ():
 	IterativeRobot (),
-	WheelFL ( 21 ),
-	WheelFR ( 23 ),
-	WheelRL ( 22 ),
-	WheelRR ( 20 ),
+	WheelFL ( 23 ),
+	WheelFR ( 20 ),
+	WheelRL ( 21 ),
+	WheelRR ( 22 ),
 	Drive ( & WheelFL, & WheelFR, & WheelRL, & WheelRR ),
 	StrafeStick ( 0 ),
 	RotateStick ( 1 )
 {	
 	
 	Drive.SetInverted ( false, true, false, true );
-	Drive.SetMotorScale ( 1000 );
+	Drive.SetMotorScale ( 10 );
 	
-	WheelFL.SetSpeedMode ( CANJaguar :: QuadEncoder, 256, 0.95, 0.01, 0.005 );
+	/*WheelFL.SetSpeedMode ( CANJaguar :: QuadEncoder, 256, 0.95, 0.01, 0.005 );
 	WheelFR.SetSpeedMode ( CANJaguar :: QuadEncoder, 256, 0.95, 0.01, 0.005 );
 	WheelRL.SetSpeedMode ( CANJaguar :: QuadEncoder, 256, 0.95, 0.01, 0.005 );
-	WheelRR.SetSpeedMode ( CANJaguar :: QuadEncoder, 256, 0.95, 0.01, 0.005 );
+	WheelRR.SetSpeedMode ( CANJaguar :: QuadEncoder, 256, 0.95, 0.01, 0.005 );*/
+	
+	WheelFL.SetVoltageMode ();
+	WheelFR.SetVoltageMode ();
+	WheelRL.SetVoltageMode ();
+	WheelRR.SetVoltageMode ();
 	
 	WheelFL.Set ( 0 );
 	WheelFR.Set ( 0 );
@@ -28,6 +33,8 @@ DevBot :: DevBot ():
 	WheelFR.EnableControl ();
 	WheelRL.EnableControl ();
 	WheelRR.EnableControl ();
+	
+	
 	
 };
 
