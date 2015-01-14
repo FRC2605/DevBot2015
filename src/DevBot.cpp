e60b869f22af64be9a91ec9e0544c005ee38fec2
Benjamin McKinney <b-mck@users.noreply.github.com>
2015-01-13 20:32:39 -0800

    totally tweaked to do fake servo testing

--- src/DevBot.cpp
+++ src/DevBot.cpp
@@ -1,26 +1,29 @@
 #include "DevBot.h"
 
+static int lastCount;
+static double lastTime;
+static double integralError;
+
 DevBot :: DevBot ():
 	IterativeRobot (),
 	WheelFL ( 21 ),
 	WheelFR ( 23 ),
 	WheelRL ( 22 ),
 	WheelRR ( 20 ),
-	VProfile ( 2.0 ),
 	Drive ( & WheelFL, & WheelFR, & WheelRL, & WheelRR ),
+	TestMotor ( 0 ),
+	TestCounter ( 0 ),
 	StrafeStick ( 0 ),
-	RotateStick ( 1 ),
-	Lift ( 0 )
+	RotateStick ( 1 )
 {	
 	
-	Drive.SetInverted ( false, true, true, true );
+	Drive.SetInverted ( false, true, false, true );
 	Drive.SetMotorScale ( 1000 );
-	Drive.AddMagDirFilter ( & VProfile );
-
-	WheelFL.SetSpeedMode ( CANJaguar :: QuadEncoder, 1000, 0.65, 0.017, 0.001 );
-	WheelFR.SetSpeedMode ( CANJaguar :: QuadEncoder, 1000, 0.65, 0.017, 0.001 );
-	WheelRL.SetSpeedMode ( CANJaguar :: QuadEncoder, 1000, 0.65, 0.017, 0.001 );
-	WheelRR.SetSpeedMode ( CANJaguar :: QuadEncoder, 1000, 0.65, 0.017, 0.001 );
+	
+	WheelFL.SetSpeedMode ( CANJaguar :: QuadEncoder, 256, 0.95, 0.01, 0.005 );
+	WheelFR.SetSpeedMode ( CANJaguar :: QuadEncoder, 256, 0.95, 0.01, 0.005 );
+	WheelRL.SetSpeedMode ( CANJaguar :: QuadEncoder, 256, 0.95, 0.01, 0.005 );
+	WheelRR.SetSpeedMode ( CANJaguar :: QuadEncoder, 256, 0.95, 0.01, 0.005 );
 	
 	WheelFL.Set ( 0 );
 	WheelFR.Set ( 0 );
@@ -32,42 +35,70 @@ DevBot :: DevBot ():
 	WheelRL.EnableControl ();
 	WheelRR.EnableControl ();
 	
-	
-	
 };
 
 DevBot :: ~DevBot ()
 {
 };
 
-void DevBot :: TeleopInit ()
+void DevBot::TeleopInit()
 {
-	
-	Drive.Enable ();
-	
-	WheelFL.EnableControl ();
-	WheelFR.EnableControl ();
-	WheelRL.EnableControl ();
-	WheelRR.EnableControl ();
-	
-};
+	lastCount = 0;
+	lastTime = GetTime();
+	integralError = 0;
+	TestCounter.Reset();
+	TestCounter.SetMaxPeriod(0.1);
+}
 
-void DevBot :: TeleopPeriodic ()
+void DevBot::TeleopPeriodic()
 {
+//	TestMotor.Set(0.5);
+	double commandVelocity = StrafeStick.GetRawAxis(1) * 25000.00;
+	if (commandVelocity <= 0.0) {
+		integralError = 0.0;
+	}
+	double kp = 0.00005;
+	double ki = 0.00005;
+	int count = TestCounter.Get();
+	double time = GetTime();
+	int sinceLastCount = count - lastCount;
+	double sinceLastTime = time - lastTime;
+	double velocity = sinceLastCount / sinceLastTime;
+	double velocityError = commandVelocity - velocity;
+	integralError += velocityError;
+	SmartDashboard::PutNumber("velocity", velocity);
+	SmartDashboard::PutNumber("since last count", sinceLastCount);
+	SmartDashboard::PutNumber("commandVelocity", commandVelocity);
+	double outputPWM = kp * (commandVelocity - velocity) + ki * integralError;
+	outputPWM = outputPWM < 0.0 ? 0.0 : outputPWM;
+	outputPWM = outputPWM > 1.0 ? 1.0 : outputPWM;
+	TestMotor.Set(outputPWM);
 
-	Drive.SetTranslation ( StrafeStick.GetX () , - StrafeStick.GetY () );
-	Drive.SetRotation ( RotateStick.GetX () );
-	
-	if ( ( StrafeStick.GetRawButton ( 2 ) != 0 ) && ( StrafeStick.GetRawButton ( 3 ) == 0 ) )
-		Lift.Set ( 1.0 );
-	else if ( ( StrafeStick.GetRawButton ( 2 ) == 0 ) && ( StrafeStick.GetRawButton ( 3 ) != 0 ) )
-		Lift.Set ( - 1.0 );
-	else
-		Lift.Set ( 0.0 );
-	
-	Drive.PushTransform ();
-	
-};
+	lastCount = count;
+	lastTime = time;
+}
+
+//void DevBot :: TeleopInit ()
+//{
+//
+//	Drive.Enable ();
+//
+//	WheelFL.EnableControl ();
+//	WheelFR.EnableControl ();
+//	WheelRL.EnableControl ();
+//	WheelRR.EnableControl ();
+//
+//};
+//
+//void DevBot :: TeleopPeriodic ()
+//{
+//
+//	Drive.SetTranslation ( StrafeStick.GetX () , - StrafeStick.GetY () );
+//	Drive.SetRotation ( RotateStick.GetX () );
+//
+//	Drive.PushTransform ();
+//
+//};
 
 void DevBot :: DisabledInit ()
 {
